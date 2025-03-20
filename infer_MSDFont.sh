9277cf7f1f5389db283c97eb6ec40de68dc90a9f
陶飞 <541003193@qq.com>
2025-03-20 18:25:40 +0800

    调整目录

--- infer_MSDFont.sh
+++ infer_MSDFont.sh
@@ -5,6 +5,6 @@ CUDA_VISIBLE_DEVICES=3 python scripts/MSDFont_eval.py \
 --path_ttf FontData/chn/ttfs/infer_unseen_font \
 --source_path FontData/chn/source.ttf \
 --path_config_rec configs/MSDFont/MSDFont_Eval_rec_model_predx0_miniUnet.yaml \
---path_rec_model logs/rec_stage2_epoch=79.ckpt \
+--path_rec_model StableDiffusion/logs/stage2/checkpoints/last.ckpt \
 --path_config_trans configs/MSDFont/MSDFont_Eval_trans_model_predx0_miniUnet.yaml \
---path_trans_model logs/trans_stage_epoch=79.ckpt
+--path_trans_model StableDiffusion/logs/stage1_1/checkpoints/last.ckpt
